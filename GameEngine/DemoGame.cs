1eb630a98f8b17dabecec2fe36aa3b4be5797056
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-03-03 12:10:59 -0300

    121003032022 - T01:18

--- GameEngine/DemoGame.cs
+++ GameEngine/DemoGame.cs
@@ -10,14 +10,15 @@ namespace GameEngine
 {
     class DemoGame : Express.Gengine
     {
-        Shape2D player;
+        Sprite2D player;
         public DemoGame() : base(new Vector2(615, 515), "GameEngine Demo") { }
 
         public override void OnLoad()
         {
             BGcolor = Color.Black;
 
-            player = new Shape2D(new Vector2(10, 10), new Vector2(10, 10), "Teste");
+            //player = new Shape2D(new Vector2(10, 10), new Vector2(10, 10), "Teste");
+            player = new Sprite2D(new Vector2(10, 10), new Vector2(36, 45), "Players/Player Grey/Player_Idle", "Player");
         }
 
         public override void OnDraw()
