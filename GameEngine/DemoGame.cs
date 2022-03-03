2683a0b0da32a7169e163121ecaf278bc59273aa
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-03-03 09:04:30 -0300

    ProjectCreate - T23:32

---
+++ GameEngine/DemoGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameEngine.Express;
+
+namespace GameEngine
+{
+    class DemoGame : Express.Gengine
+    {
+        public DemoGame() : base(new Vector2(615,515), "GameEngine Demo") { }
+    }
+}
